973f0054d2555c70688bc9fd514a457ec2a2f6f4
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-07-12 18:25:49 -0700

    added date and time

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello")
+
+//add missing html elements
+
+// working clock and date with moment.js
+var date = moment().format("dddd LL");
+$("#date").append(date);
+
+var time = moment().format("LT");
+$("#time").append(time);
+
+// web API's in Jquery
+
+// time blocks can be made with the for loops in js
+
+// when user clicks time block they need to be able to type in text
+
+// text needs to be stored through local storage.
+
